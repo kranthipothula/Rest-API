32f2697e5b1e86d0e1addab5ab96cb9a5f727152
kranthipothula <150351636+kranthipothula@users.noreply.github.com>
2023-11-29 16:41:04 +0530

    Add files via upload

---
+++ api/routes/student.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/',(req,res,next)=>{
+    res.status(200).json({
+        msg: 'this is student get request'
+    })
+})
+
+router.post('/',(req,res,next)=>{
+    res.status(200).json({
+        msg: 'this is student post request'
+    })
+})
